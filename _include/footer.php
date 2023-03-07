fa36937bfbb9ccac941dfa9efce74b87e0b0e915
corentin-roussel <corentin.roussel@laplateforme.io>
2023-03-07 14:29:53 +0100

    DEV [+] 14:29 ébauche style accueil et début style formulaire

--- _include/footer.php
+++ _include/footer.php
@@ -2,25 +2,26 @@
 ?>
 
 <section class="about-blog">
-    <h3>A propos de nous</h3>
-    <p>C'est un pargraphe de test</p>
+    <h3 class="footer-title">A propos de nous</h3>
+    <p class="footer-text">C'est un pargraphe de test</p>
 </section>
 <section class="link-blog">
     <?php
         if(isset($_SESSION)) {
     ?>
-            <a class="link" href="profil.php">Profil</a>
-            <a class="link" href="article.php">Article</a>
+            <a class="footer-link" href="profil.php">Profil</a>
+            <a class="footer-link" href="article.php">Article</a>
     <?php
         }else {
     ?>
-            <a href="index.php">Accueil</a>
-            <a href="authentification.php">S'authentifier</a>
+            <a class="footer-link" href="index.php">Accueil</a>
+            <a class="footer-link" href="authentification.php">S'authentifier</a>
     <?php
         }
     ?>
 
 </section>
 <section class="contact-blog">
-
+    <h3 class="footer-title">Nous contacter</h3>
+    <p class="footer-text">Télephone: 06-06-06-06-00</p>
 </section>
--- authentification.php
+++ authentification.php
@@ -4,11 +4,7 @@
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <?php require_once('_include/head.php') ?>
     <title>Authentification</title>
 </head>
 <body>
@@ -18,7 +14,9 @@
         </article>
     </header>
     <main>
+        <article id="place">
 
+        </article>
     </main>
     <footer>
         <article id="flex-footer">
@@ -27,3 +25,35 @@
     </footer>
 </body>
 </html>
+
+<div class="inscription-container">
+    <form method="POST" id="formReg">
+        
+        <h2 class="footer-title">Inscription</h2>
+
+        <label class="space footer-text" for="login">Login : </label>
+        <input type="text" name="login" id="login">
+
+        <label class="space footer-text" for="password">Mot de passe : </label>
+        <input type="password" name="password" id="password">
+
+        <label class="space footer-text" for="confpassword">Confirmation mot de passe : </label>
+        <input type="password" name="confpassword" id="confpassword">
+
+        <input class="button-form" type="submit" value="Inscription" id="inscription">
+    </form>
+</div>
+
+<div class="connexion-container">
+    <form method="POST" id="formCon">
+        <h2 class="footer-title">Connexion</h2>
+
+        <label class="space" for="login">Login : </label>
+        <input type="text" name="login" id="login">
+
+        <label class="space" for="password">Mot de passe : </label>
+        <input type="password" name="password" id="password">
+
+        <input class="button-form" type="submit" value="Connexion" id="connexion">
+    </form>
+</div>
