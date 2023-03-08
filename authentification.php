2ba7aeb41db5a3ade3e8bd1244abfd67c9207418
corentin-roussel <corentin.roussel@laplateforme.io>
2023-03-08 09:30:06 +0100

    ADD [+] 09:30 ajout page connexion inscription

--- authentification.php
+++ authentification.php
@@ -26,34 +26,6 @@
 </body>
 </html>
 
-<div class="inscription-container">
-    <form method="POST" id="formReg">
 
-        <h2 class="register-title">Inscription</h2>
 
-        <label class="space register-text" for="login">Login</label>
-        <input class="input" type="text" name="login" id="login">
 
-        <label class="space register-text" for="password">Mot de passe</label>
-        <input class="input" type="password" name="password" id="password">
-
-        <label class="space register-text" for="confpassword">Confirmation mot de passe</label>
-        <input class="input" type="password" name="confpassword" id="confpassword">
-
-        <input class="button-form" type="submit" value="Inscription" id="inscription">
-    </form>
-</div>
-
-<div class="connexion-container">
-    <form method="POST" id="formCon">
-        <h2 class="conn-title">Connexion</h2>
-
-        <label class="space conn-text" for="login">Login</label>
-        <input class="input" type="text" name="login" id="login">
-
-        <label class="space conn-text" for="password">Mot de passe</label>
-        <input class="input" type="password" name="password" id="password">
-
-        <input class="button-form" type="submit" value="Connexion" id="connexion">
-    </form>
-</div>
---
+++ inscription.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<div class="inscription-container">
+    <form method="POST" id="formReg">
+
+        <h2 class="register-title">Inscription</h2>
+
+        <label class="space register-text" for="login">Login</label>
+        <input class="input" type="text" name="login" id="login">
+
+        <label class="space register-text" for="password">Mot de passe</label>
+        <input class="input" type="password" name="password" id="password">
+
+        <label class="space register-text" for="confpassword">Confirmation mot de passe</label>
+        <input class="input" type="password" name="confpassword" id="confpassword">
+
+        <input class="button-form" type="submit" value="Inscription" id="inscription">
+    </form>
+</div>
