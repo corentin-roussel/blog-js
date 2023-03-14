2b0ff96ac1dd3d918a7c960d65649066dbbedef7
corentin-roussel <corentin.roussel@laplateforme.io>
2023-03-14 12:19:42 +0100

    FIX [+] mise en place des focntionnalités pagination et display commentaires ensemble

--- _include/header.php
+++ _include/header.php
@@ -3,7 +3,7 @@
     if(isset($_SESSION['user'])) {
 ?>
         <a class="link" href="profil.php">Profile</a>
-        <a class="link" href="article-commentaires.php">Article</a>
+        <a class="link" href="articles-page.php">Article</a>
         <a class="link" href="deconnexion.php">Déconnexion</a>
 <?php
     }else {
