7c5547dcd269cb88ca653941d2405463593eafc4
corentin-roussel <corentin.roussel@laplateforme.io>
2023-03-20 10:25:57 +0100

    CHANGE [+] 10:25 css form commentaire

--- commentaire.php
+++ commentaire.php
@@ -3,7 +3,7 @@
 
 <form action="" method="POST" id="formComm">
     <label class="form-label" for="comment">Comment : </label>
-    <textarea class="space" name="comment" id="comment" cols="30" rows="10"></textarea>
+    <textarea class="space" name="comment" id="comment" cols="30" rows="5"></textarea>
     <div id="errorComm" class="error"></div>
 
     <input class="button-form" type="submit" name="submitComm" id="submitComm" value="Comment">
